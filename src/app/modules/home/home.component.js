2c57222f999b472aa28cce32bba4956f3c4b7a8b
= <=>
2022-04-22 17:20:19 -0300

    Create NextJS Initial Structure

---
+++ src/app/modules/home/home.component.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function HomeComponent() {
+  return (
+    <div>Hello World</div>
+  );
+}
+
+export default HomeComponent;
---
+++ src/app/modules/home/home.container.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import HomeComponent from './home.component';
+
+function HomeContainer() {
+  return (
+    <HomeComponent />
+  );
+}
+
+export default HomeContainer;
